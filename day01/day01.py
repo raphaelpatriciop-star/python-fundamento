8793908806d92814cb2c636d266d4ee996e85d40
raphaelpatriciop-star <raphaelpatriciop@gmail.com>
2025-12-31 12:57:52 -0300

    Add files via upload

---
+++ day01/day01.py
@@ -0,0 +1,7 @@
+nome =input ("Digite seu Nome:  ")
+ano = int(input("Digite o ano "))
+anona = int(input ("Digite o ano nascimento"))
+idade = int (ano-anona)
+print ("ola mundo aqui esta seus dados")
+print ("Nome", nome)
+print ("Idade", idade)
---
+++ day01/rodizio.py
@@ -0,0 +1,23 @@
+print ("Ola Seja bem vindo Vou informar o dia de rodizio de seu veiculo")
+print ("Por favor Digite a Sua Placa")
+placa = input("Digite sua placa:")
+
+digito = placa[-1]
+if not digito.isdigit():
+        print ("placa invalida")
+else :
+        dia = int(digito) 
+        semana = ""
+        if dia == 1 or dia == 2:
+         semana = ("segunda-feira")
+        elif dia == 3 or dia == 4:
+         semana = ("terca-feira")
+        elif dia == 5 or dia == 6 :
+         semana = ("quarta-feira")
+        elif dia == 7 or dia == 8:
+         semana = ("quinta feira")
+        elif dia == 9 or dia == 0 :
+         semana = ("sexta feira") 
+
+        print ("Aqui esta seu dia de rodizio")
+        print ( "O seu dia da semana de rodizio é", semana)
