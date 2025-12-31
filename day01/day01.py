38e22945f762d4ef0a967ba2174a3f5fe9e08bd1
raphaelpatriciop-star <raphaelpatriciop@gmail.com>
2025-12-31 13:02:40 -0300

    Enhance age calculation and add metadata
    Added age calculation and improved comments.

--- day01/day01.py
+++ day01/day01.py
@@ -1,7 +1,13 @@
+# Programa para calcular idade
+# Autor: Raphael Patricio Pereira
+# Data: 29/12/2025
+
+
 nome =input ("Digite seu Nome:  ")
 ano = int(input("Digite o ano "))
 anona = int(input ("Digite o ano nascimento"))
 idade = int (ano-anona)
 print ("ola mundo aqui esta seus dados")
 print ("Nome", nome)
+
 print ("Idade", idade)
