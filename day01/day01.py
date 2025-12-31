31076c3e7354d87545fa2a879636466ab77b3668
raphaelpatriciop-star <raphaelpatriciop@gmail.com>
2025-12-31 13:20:11 -0300

    Update day01.py

--- day01/day01.py
+++ day01/day01.py
@@ -2,12 +2,14 @@
 # Autor: Raphael Patricio Pereira
 # Data: 29/12/2025
 
+# Entrada das Variaveis 
+nome =input ("Digite seu Nome:  ") # variavel tipo (string)
+ano = int(input("Digite o ano "))  # variavel tipo (inteiro)
+anona = int(input ("Digite o ano nascimento")) # variavel tipo (inteiro)
+idade = int (ano-anona) # variavel tipo (inteiro) que vai recceber o valor do ano menos o anona 
 
-nome =input ("Digite seu Nome:  ")
-ano = int(input("Digite o ano "))
-anona = int(input ("Digite o ano nascimento"))
-idade = int (ano-anona)
-print ("ola mundo aqui esta seus dados")
+# exibe os dados na tela
+print ("ola mundo aqui esta seus dados") # graça do programador rsrs 
 print ("Nome", nome)
-
 print ("Idade", idade)
+
